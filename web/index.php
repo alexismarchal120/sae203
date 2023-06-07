967899d313319080806d3cbbd7bb4f9e8f34ba70
alexismarchal120 <alexismarchal120@gmail.com>
2023-06-07 14:12:40 +0200

    finnalyti

--- web/index.php
+++ web/index.php
@@ -6,19 +6,26 @@
 </head>
 <body>
     <canvas id="myChart"></canvas>
-
+    <canvas id="test"></canvas>
     <script>
         var ctx = document.getElementById('myChart').getContext('2d');
 
         <?php
-        $db = new SQLite3('/basededonnee/sae203.db');
+        $db = new SQLite3('../basededonnee/sae203.db');
         $query = $db->query('SELECT * FROM temperature');
         $data = array();
         $labels = array();
 
+        $query1 = $db->query('SELECT * FROM hummydite');
+        $data1 = array();
+        $labels1 = array();
         while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
-            $data[] = $row['jour']."/".$row['mois']."/".$row['annee'];
-            $labels[] = $row['temp'];
+            $data[] = $row['temp'];
+            $labels[] = $row['jour']."/".$row['mois']."/".$row['annee'];
+        }
+        while ($row = $query1->fetchArray(SQLITE3_ASSOC)) {
+            $data1[] = $row['taux'];
+            $labels1[] = $row['jour']."/".$row['mois']."/".$row['annee'];
         }
         ?>
 
@@ -26,12 +33,52 @@
         var labels = <?php echo json_encode($labels); ?>;
 
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
                 labels: labels,
                 datasets: [{
-                    label: 'Mon graphique',
+                    label: 'température',
                     data: data,
+                    backgroundColor: 'rgba(0, 123, 255, 0.5)'
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+    <script>
+        var ctx = document.getElementById('test').getContext('2d');
+
+        
+        
+        <?php
+        $db = new SQLite3('../basededonnee/sae203.db');
+        $query1 = $db->query('SELECT * FROM hummydite');
+        $data1 = array();
+        $labels1 = array();
+        
+        while ($row = $query1->fetchArray(SQLITE3_ASSOC)) {
+            $data1[] = $row['taux'];
+            $labels1[] = $row['jour']."/".$row['mois']."/".$row['annee'];
+        }
+        ?>
+
+        var data1 = <?php echo json_encode($data1); ?>;
+        var labels1 = <?php echo json_encode($labels1); ?>;
+
+        var test = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels1,
+                datasets: [{
+                    label: 'humidyte',
+                    data: data1,
                     backgroundColor: 'rgba(0, 123, 255, 0.5)'
                 }]
             },
